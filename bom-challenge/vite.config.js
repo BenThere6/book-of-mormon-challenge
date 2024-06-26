0ef4eadcf1a6d5c1f53f3cc57d71690954574489
BenThere <benjaminbirdsall@icloud.com>
2024-06-25 18:39:00 -0600

    update configs

--- bom-challenge/vite.config.js
+++ bom-challenge/vite.config.js
@@ -9,6 +9,11 @@ export default defineConfig({
       input: {
         main: 'index.html',
       },
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+      },
     },
   },
 });

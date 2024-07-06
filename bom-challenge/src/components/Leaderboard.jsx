116f037f14a17488ef966ebf3808d3f44171f261
BenThere <benjaminbirdsall@icloud.com>
2024-07-05 21:02:23 -0600

    fix leaderboard bug!!!

--- bom-challenge/src/components/Leaderboard.jsx
+++ bom-challenge/src/components/Leaderboard.jsx
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import '../assets/css/Leaderboard.css';
 import Button from '@mui/material/Button';
+
 let apiurl;
-if (process.env.NODE_ENV == 'development') {
+if (process.env.NODE_ENV === 'development') {
   apiurl = 'http://localhost:3000/leaderboard';
 } else {
   apiurl = 'https://bens-api-dd63362f50db.herokuapp.com/leaderboard';
@@ -18,20 +19,17 @@ function Leaderboard() {
   const [username, setUsername] = useState('');
   const [userRank, setUserRank] = useState(null);
   const [isScoreSubmitted, setIsScoreSubmitted] = useState(false);
-  const [isScoreSubmitting, setIsScoreSubmitting] = useState(false); // New state to track score submission
+  const isSubmittingRef = useRef(false);
 
   useEffect(() => {
-    // Retrieve username from local storage
     const storedUsername = localStorage.getItem('username');
-    if (storedUsername && !isScoreSubmitted && !isScoreSubmitting) {
+    if (storedUsername && !isScoreSubmitted && !isSubmittingRef.current) {
       setUsername(storedUsername);
-      setIsScoreSubmitting(true); // Set flag to indicate score submission is in progress
-      // Automatically submit score if username is retrieved and not already submitted
+      isSubmittingRef.current = true; // Set flag to indicate score submission is in progress
       submitScore(storedUsername, score);
     }
-
     fetchLeaderboard();
-  }, [score, isScoreSubmitted, isScoreSubmitting]);
+  }, [isScoreSubmitted, score]);
 
   const fetchLeaderboard = () => {
     fetch(apiurl)
@@ -51,23 +49,18 @@ function Leaderboard() {
   };
 
   const submitScore = (username, score) => {
-    // Retrieve game IDs from localStorage
     const storedGameIDs = localStorage.getItem('gameIDs');
     let gameIDs = storedGameIDs ? JSON.parse(storedGameIDs) : {};
 
-    // Find the latest game ID
     const latestGameID = Object.keys(gameIDs).length > 0 ? Math.max(...Object.keys(gameIDs)) : null;
-    console.log('latest game id: ' + latestGameID)
-    // Check if latest game ID exists and its status
-    if (latestGameID !== null) {
-      const latestGameIDStatus = gameIDs[latestGameID];
-      if (latestGameIDStatus === true) {
-        console.log(`Score for game ID ${latestGameID} already submitted.`);
-        return;
-      }
+    console.log('latest game id: ' + latestGameID);
+
+    if (latestGameID !== null && gameIDs[latestGameID] === true) {
+      console.log(`Score for game ID ${latestGameID} already submitted.`);
+      isSubmittingRef.current = false; // Reset flag if score is already submitted
+      return;
     }
 
-    // Proceed to submit score
     console.log('Submitting score:', { username, score });
 
     fetch(apiurl, {
@@ -84,26 +77,25 @@ function Leaderboard() {
       .then((data) => {
         console.log('Score saved, response:', data);
 
-        setIsScoreSubmitted(true); // Set score submission flag to true
-        setIsScoreSubmitting(false); // Reset score submission in progress flag
-        fetchLeaderboard(); // Fetch leaderboard again to update data
+        setIsScoreSubmitted(true);
+        isSubmittingRef.current = false; // Reset flag after submission
 
-        // Update game ID status in localStorage to true
         if (latestGameID !== null) {
           gameIDs[latestGameID] = true;
           localStorage.setItem('gameIDs', JSON.stringify(gameIDs));
         }
+
+        fetchLeaderboard();
       })
       .catch((error) => {
-        setIsScoreSubmitting(false); // Reset score submission in progress flag on error
         console.error('Error saving score:', error);
+        isSubmittingRef.current = false; // Reset flag on error
       });
   };
 
   const handlePlayAgain = () => {
     setUsername('');
     setIsScoreSubmitted(false);
-    setIsScoreSubmitting(false); // Reset score submission in progress flag
     navigate('/'); // Navigate to the start screen
   };
 
@@ -131,11 +123,9 @@ function Leaderboard() {
             </li>
           ))}
         </ol>
-        {(
-          <div className="play-again">
-            <Button variant="contained" onClick={handlePlayAgain}>Play Again</Button>
-          </div>
-        )}
+        <div className="play-again">
+          <Button variant="contained" onClick={handlePlayAgain}>Play Again</Button>
+        </div>
       </div>
     </div>
   );

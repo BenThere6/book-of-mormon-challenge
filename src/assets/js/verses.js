f4ff83d83102f0cac28859bd510cdae905158e5c
BenThere <benjaminbirdsall@icloud.com>
2024-08-13 02:25:09 -0600

    added scripture mastery to easy

--- src/assets/js/verses.js
+++ src/assets/js/verses.js
@@ -1,3 +1,6 @@
+import scriptureMasteryVerses from './scriptureMasteryVerses';
+
+
 const verses = {
   "1 Nephi 1:1": "I, Nephi, having been born of goodly parents, therefore I was taught somewhat in all the learning of my father; and having seen many afflictions in the course of my days, nevertheless, having been highly favored of the Lord in all my days; yea, having had a great knowledge of the goodness and the mysteries of God, therefore I make a record of my proceedings in my days.",
   "1 Nephi 1:2": "Yea, I make a record in the language of my father, which consists of the learning of the Jews and the language of the Egyptians.",
@@ -6901,5 +6904,8 @@ for (const [reference, verse] of Object.entries(verses)) {
     extremeVerses[reference] = verse;
   }
 }
+
+// Merge scriptureMasteryVerses into easyVerses
+Object.assign(easyVerses, scriptureMasteryVerses);
 
 export { easyVerses, mediumVerses, hardVerses, extremeVerses };
